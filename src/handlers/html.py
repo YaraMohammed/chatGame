33acc1bfb89129c0d2069fda6145a76c4d65119f
MariemOsama <mariem.osama152@gmail.com>
2017-02-26 08:10:37 +0200

    signup and register data to mongodb

--- src/handlers/html.py
+++ src/handlers/html.py
@@ -1,6 +1,28 @@
-from tornado import web
+import pymongo
+from tornado import web , escape
+from pymongo import MongoClient
+
+
 
 
 class MainHandler(web.RequestHandler):
     def get(self):
-        self.write("Working")
+        self.write("Working");
+
+class SignUpHandler(web.RequestHandler):
+    def post(self):
+        username = self.get_body_argument("username");
+        firstName = self.get_body_argument("firstName");
+        password = self.get_body_argument("password");
+        self.write(str(username));
+
+        client = MongoClient();
+        client.chatGame.users.insert_one(
+        {
+        "_id":username,
+        "name":firstName,
+        "password":password,
+        "groups":[],
+        "friends":[]
+        }
+        );
