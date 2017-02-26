367323ffb4adeba1afcf349e3fa29f440d62e86d
Ahmed Mokhtar <mokhtar92@gmail.com>
2017-02-26 19:42:55 +0200

    following PEP 8 style guidelines

--- src/handlers/html.py
+++ src/handlers/html.py
@@ -1,28 +1,24 @@
-import pymongo
-from tornado import web , escape
+from tornado import web
 from pymongo import MongoClient
-
-
 
 
 class MainHandler(web.RequestHandler):
     def get(self):
-        self.write("Working");
+        self.write("Working")
+
 
 class SignUpHandler(web.RequestHandler):
     def post(self):
-        username = self.get_body_argument("username");
-        firstName = self.get_body_argument("firstName");
-        password = self.get_body_argument("password");
-        self.write(str(username));
+        username = self.get_body_argument("username")
+        firstName = self.get_body_argument("firstName")
+        password = self.get_body_argument("password")
+        self.write(str(username))
 
-        client = MongoClient();
-        client.chatGame.users.insert_one(
-        {
-        "_id":username,
-        "name":firstName,
-        "password":password,
-        "groups":[],
-        "friends":[]
-        }
-        );
+        client = MongoClient()
+        client.chatGame.users.insert_one({
+            "_id": username,
+            "name": firstName,
+            "password": password,
+            "groups": [],
+            "friends": []
+        })
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 from tornado import web, ioloop
-from handlers.html import MainHandler ,SignUpHandler
+from handlers.html import MainHandler, SignUpHandler
 from handlers.ws import WSHandler
 
 app = web.Application([
     (r'/', MainHandler),
     (r'/ws', WSHandler),
-    (r'/signup',SignUpHandler)
+    (r'/signup', SignUpHandler)
 ], static_path='static', debug=True)
 
 app.listen(8888)
