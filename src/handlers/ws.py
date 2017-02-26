e813b82bbd7a054909d5a1e2f636d2f34f49f2c7
Ahmed Mokhtar <mokhtar92@gmail.com>
2017-02-26 21:43:36 +0200

    initial ws context tracking

--- src/handlers/ws.py
+++ src/handlers/ws.py
@@ -1,15 +1,45 @@
 from tornado import websocket
+import json
 from jose import jwt
 
 chat_rooms = {}
 
 
 class WSHandler(websocket.WebSocketHandler):
+    username = ''
+    room = ''
+
     def open(self):
         print('WebSocket Opened!')
 
     def on_message(self, msg):
-        self.write_message('u said ' + msg)
+        # TODO: check malformed json
+        msg = json.loads(msg)
+
+        # TODO: check integrity between `type` and other message keys
+        if msg['type'] == 'authenticate':
+            # TODO: handle jwt signature error
+            token = jwt.decode(msg['token'], 'secret', algorithms=['HS256'])
+            self.username = token['username']
+
+        elif msg['type'] == 'setRoom':
+            # TODO: check if chat room exists in db
+            if msg['room'] not in chat_rooms:
+                chat_rooms[msg['room']] = {self}
+            else:
+                chat_rooms[msg['room']].add(self)
+
+            print(chat_rooms)
+
+            if self.room != '':
+                chat_rooms[self.room].discard(self)
+
+            self.room = msg['room']
 
     def on_close(self):
+        if self.room != '':
+            chat_rooms[self.room].discard(self)
+
+        print(chat_rooms)
+
         print('WebSocket Closed!')
